2b6bac52d5f46e25b8c9bce6f858c2939ba927d5
MyCloudream <chenjun_work@163.com>
2017-03-05 01:01:01 +0800

    定时任务添加

--- src/com/oucre/quartz/MyQuartz.java
+++ src/com/oucre/quartz/MyQuartz.java
@@ -1,11 +1,35 @@
 package com.oucre.quartz;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.oucre.dao.StudentDao;
+import com.oucre.pojo.Student;
 
 public class MyQuartz {
 	private Logger logger = Logger.getLogger(this.getClass());
+	@Autowired
+	private StudentDao studentDao;
+
 	public void clearimgtemp() {
-/*		System.out.println("执行清理方法");*/
-		logger.error("-------------执行定时清理方法 execute timer clear method-----------");
+		try {
+			List<Student> list = studentDao.findAllStudents();
+			for (Student s : list) {
+				// 审核通过状态 并且是 自己转化 时，开启转化时间倒计时
+				if ("2".equals(s.getStatus()) && "1".equals(s.getConver())) {
+					int days = s.getDays() - 1;
+					if (days == 0) {
+						s.setConver("2");
+					}
+					s.setDays(days);
+					studentDao.upd(s);
+				}
+			}
+		} catch (Exception e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
 	}
 }
